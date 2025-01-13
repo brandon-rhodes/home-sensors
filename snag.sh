fa1f741f654cc7a1bcc08816c6d65a2395d9a636
Brandon Rhodes <brandon@rhodesmill.org>
2025-01-12 20:49:44 -0500

    First data points from 'office' location

--- snag.sh
+++ snag.sh
@@ -3,7 +3,7 @@
 set -e
 cd "$(readlink -f $(dirname "${BASH_SOURCE[0]}"))"
 
-for host in attic basement
+for host in basement office
 do
     mkdir -p data-$host
     rsync -aiv $host:'data-*' data-$host/
