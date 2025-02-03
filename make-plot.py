7732096a0ca00e63d47dbe697889cdf9b697b47a
Brandon Rhodes <brandon@rhodesmill.org>
2025-02-02 22:13:12 -0500

    Compute the SGP40 VOC index, in retrospect

--- make-plot.py
+++ make-plot.py
@@ -3,6 +3,7 @@
 # /// script
 # requires-python = ">=3.8"
 # dependencies = [
+#     "adafruit-circuitpython-sgp40",
 #     "plotly",
 # ]
 # ///
@@ -13,12 +14,15 @@ from collections import defaultdict
 from datetime import datetime
 from glob import glob
 
+#pattern = 'data-basement/data-*'
+pattern = 'data-office/data-*'
+
 t = []
 data = defaultdict(list)
 
 # Each line of data looks like: 2024-11-11 18:14 F=64.7 H%=63.1
 
-for path in sorted(glob('data-basement/data-*')):
+for path in sorted(glob(pattern)):
     print(path)
     for line in open(path):
         line = line.replace('\000', '')  # how did nulls get in there?
@@ -35,7 +39,40 @@ for path in sorted(glob('data-basement/data-*')):
 
 data = dict(data)
 
+# fig = go.Figure()
+# fig.add_trace(go.Scatter(x=t, y=data['F'], name='Temperature (°F)'))
+# fig.add_trace(go.Scatter(x=t, y=data['H%'], name='Humidity (%)'))
+# fig.show()
+
+# fig = go.Figure()
+# fig.add_trace(go.Scatter(x=t, y=data['CO2_ppm'], name='VOC_raw'))
+# fig.show()
+
+# fig = go.Figure()
+# fig.add_trace(go.Scatter(x=t, y=data['voc_raw'], name='VOC raw'))
+# fig.show()
+
+from adafruit_sgp40 import SGP40
+
+class SGP40_offline(SGP40):
+    def __init__(self):
+        self._voc_algorithm = None
+    def set_next_raw_measurement(self, value):
+        self.datum = value
+    def measure_raw(self, temperature, relative_humidity):
+        return self.datum
+
+def compute_voc_index():
+    harness = SGP40_offline()
+    series = []
+    for item in data['voc_raw']:
+        harness.set_next_raw_measurement(item)
+        voc = harness.measure_index(None, None)
+        series.append(voc)
+    return series
+
+voc_index = compute_voc_index()
+
 fig = go.Figure()
-fig.add_trace(go.Scatter(x=t, y=data['F'], name='Temperature (°F)'))
-fig.add_trace(go.Scatter(x=t, y=data['H%'], name='Humidity (%)'))
+fig.add_trace(go.Scatter(x=t, y=voc_index, name='VOC index'))
 fig.show()
