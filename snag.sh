59a9fd8f5d4ffb505e42ec2eec71bf98958ffde9
Brandon Rhodes <brandon@rhodesmill.org>
2025-03-29 10:22:43 -0400

    Have snag.sh display each host it tries to contact
    Otherwise, when I see an error I won’t know which host it was about.

--- snag.sh
+++ snag.sh
@@ -5,6 +5,7 @@ cd "$(readlink -f $(dirname "${BASH_SOURCE[0]}"))"
 
 for host in basement office
 do
+    echo "Host: $host"
     mkdir -p data-$host
     rsync -aiv $host:'data-*' data-$host/
 done
